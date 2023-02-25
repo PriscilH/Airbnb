72765d626d9517f48a0d88ffde68c12bd7b1e77e
Priscilla <comptepro@mbpdepriscilla.home>
2023-02-25 16:50:16 +0100

    add Logo and css input

--- containers/SignInScreen.js
+++ containers/SignInScreen.js
@@ -1,15 +1,24 @@
 import { useNavigation } from "@react-navigation/core";
-import { Button, Text, TextInput, View, TouchableOpacity } from "react-native";
+import { Button, Text, TextInput, View, Image, TouchableOpacity, StyleSheet } from "react-native";
+import Logo from "../assets/airbnb-icon.png";
 
 export default function SignInScreen({ setToken }) {
   const navigation = useNavigation();
+
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.align}>
+      <Image source={Logo} style={styles.logo}/>
+      <Text style={styles.sign}>Sign in</Text>
+      </View>
       <View>
-        <Text>Name: </Text>
-        <TextInput placeholder="Username" />
-        <Text>Password: </Text>
-        <TextInput placeholder="Password" secureTextEntry={true} />
+        <View style={styles.block} >
+          {/* <Text>Name: </Text> */}
+        <View style={styles.input}><TextInput placeholder="Email" /></View>
+        {/* <Text>Password: </Text> */}
+        <View style={styles.input}><TextInput placeholder="Password" secureTextEntry={true} /></View>
+        </View>
+        
         <Button
           title="Sign in"
           onPress={async () => {
@@ -28,3 +37,35 @@ export default function SignInScreen({ setToken }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  logo:{
+    width: 100,
+    height: 100,
+    marginTop: 70,
+    marginBottom: 25,
+  },
+  align:{
+    alignItems: "center",
+    marginBottom: 100,
+  },
+  sign:{
+    fontSize: 25,
+    color: '#717171',
+    fontWeight: "bold",
+  },
+  block:{
+    marginLeft: 35,
+    marginRight: 35,
+  },
+  input:{
+    borderBottomWidth: 1,
+    borderBottomColor: '#EB5A62',
+    marginBottom: 25,
+    paddingBottom: 5,
+  },
+});
