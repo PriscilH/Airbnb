e437db56ecb696fd7a9fd58db4a978132c0deaad
Priscilla <comptepro@mbpdepriscilla.home>
2023-03-02 13:15:26 +0100

    update SignUpScreen

--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -1,10 +1,41 @@
-import { Button, Text, TextInput, View, Image, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import { Text, TextInput, View, Image, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import Logo from "../assets/airbnb-icon.png"
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useNavigation } from "@react-navigation/core";
+import { useState } from "react";
+import axios from "axios";
 
-export default function SignUpScreen({ setToken }) {
-  const navigation = useNavigation();
+export default function SignUpScreen({ setToken, navigation }) {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async () => {
+    // console.log({ email, username, description, password, confirmPassword });
+    if (email && username && description && password && confirmPassword) {
+      if (password === confirmPassword) {
+        try {
+          const { data } = await axios.post(
+            "https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/user/sign_up",
+            { email, username, description, password }
+          );
+
+          console.log("response>>", data);
+          alert(data.token);
+        } catch (error) {
+          console.log("catch>>", error.response.data);
+          setErrorMessage("An error occured, this email already exist !");
+        }
+      } else {
+        setErrorMessage("Passwords must be the same");
+      }
+    } else {
+      setErrorMessage("Please fill all fields");
+    }
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -16,26 +47,43 @@ export default function SignUpScreen({ setToken }) {
 
       <KeyboardAwareScrollView>
       <View style={styles.block}>
-        <TextInput style={styles.input} placeholder="Email" />
-        <TextInput style={styles.input} placeholder="Username"  />
+        <TextInput style={styles.input} placeholder="Email" onChangeText={(text) => {
+          // réinitialisation du message d'erreur dès que l'utilisateur change la valeur de l'input
+          setErrorMessage("");
+          setEmail(text);
+        }} />
+        <TextInput style={styles.input} placeholder="Username" onChangeText={(text) => {
+          setErrorMessage("");
+          setUsername(text);
+        }}  />
         <TextInput style={styles.area} multiline = {true} numberOfLines = {4}
-        placeholder="Describe yourself in a few words..."/>
-        <TextInput style={styles.input} placeholder="Password" secureTextEntry={true}/>
-        <TextInput style={styles.input} placeholder="Confirm password" secureTextEntry={true} />
+        placeholder="Describe yourself in a few words..." onChangeText={(text) => {
+          setDescription(text);
+        }}/>
+        <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} 
+        onChangeText={(text) => {
+          setErrorMessage("");
+          setPassword(text);
+        }}/>
+        <TextInput style={styles.input} placeholder="Confirm password" secureTextEntry={true}
+        onChangeText={(text) => {
+          setErrorMessage("");
+          setConfirmPassword(text);
+        }} />
+        {/* Affichage du message d'erreur */}
+      {errorMessage && <Text>{errorMessage}</Text>}
       </View> 
       </KeyboardAwareScrollView> 
 
       {/* BUTTON SIGN UP */}
 
-      <View style={styles.align2}>
+      <TouchableOpacity style={styles.align2}>
         <View style={styles.border}>
         <Text style={styles.button}
-          onPress={async () => {
-            const userToken = "secret-token";
-            setToken(userToken);
-          }}>Sign up</Text>
+          onPress={handleSubmit}
+          >Sign up</Text>
         </View> 
-      </View>
+      </TouchableOpacity>
         
       <TouchableOpacity
           onPress={() => {
@@ -46,7 +94,6 @@ export default function SignUpScreen({ setToken }) {
            <Text style={styles.subsentence}>Already have an account ? Sign in</Text> 
           </View>
       </TouchableOpacity>
-      
     </ScrollView>
   );
 }
